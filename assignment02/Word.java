d4a3a1e484ddcd23fc2641d6cb77efc8bc8d2369
kevinMaynard20 <46655789+kevinMaynard20@users.noreply.github.com>
2024-02-25 13:29:01 -0500

    Word.java complete

--- assignment02/Word.java
+++ assignment02/Word.java
@@ -16,7 +16,31 @@ public class Word extends Decorator {
 		// include y if thisWord is in Ywords.hasYconsonant
 		// you may find new words that have to be addes 
 		// to the file yconsonant.txt
-		return null;
+		
+		
+		Set<Character> consonants = Set.of(
+   		 'b', 'c', 'd', 'f', 'g', 
+    		 'h', 'j', 'k', 'l', 'm', 
+   		 'n', 'p', 'q', 'r', 's', 
+  		 't', 'v', 'w', 'x', 'z');
+
+		String temp = thisWord.toLowerCase();
+		 if (assignment02.Ywords.hasYconsonant.contains(temp)) {
+         		consonants = new HashSet<>(consonants); // Convert to mutable set if using Set.of
+            		consonants.add('y');
+        	}
+		
+		Set<Character> overlap = new HashSet<>();
+
+		for (int i = 0; i < temp.length(); i++) {
+            		char ch = temp.charAt(i);
+           	 if (consonants.contains(ch)) 
+		 	{
+               		 overlap.add(ch);
+            		}
+      		}
+		
+		return overlap;
 	}
 	@Override
 	public Set<Character> getVowelsInWord() {
@@ -25,7 +49,28 @@ public class Word extends Decorator {
 		// include y if thisWord is in Ywords.hasYvowel
 		// you may find new words that have to be addes 
 		// to the file yconsonant.txt
-		return null;
+		
+		
+		Set<Character> vowels = Set.of(
+   		 'a', 'e', 'i', 'o', 'u');
+		String temp = thisWord.toLowerCase();
+		 if (assignment02.Ywords.hasYvowel.contains(temp)) {
+         		vowels = new HashSet<>(vowels); // Convert to mutable set if using Set.of
+            		vowels.add('y');
+        	}
+
+		
+		Set<Character> overlap = new HashSet<>();
+
+		for (int i = 0; i < temp.length(); i++) {
+            		char ch = temp.charAt(i);
+           	 if (vowels.contains(ch)) 
+		 	{
+               		 overlap.add(ch);
+            		}
+      		}
+		
+		return overlap;
 	}
 
 	@Override
